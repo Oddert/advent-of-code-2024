bd81e778b08c9019c691aa52da7bb4ff55ff7ac3
Robyn F H Veitch <oddert@gmail.com>
2024-12-18 12:04:06 +0000

    feat: added day 9 p1

---
+++ 9/index.py
@@ -0,0 +1,130 @@
+import sys
+import re
+
+from typing import Tuple, List
+
+# Needed to import from folder directory. Replace with your own project.
+# Probably a better way to do this that doesn't involve revealing my own file structure but whatever, you're not my mum.
+sys.path.append(r'C:\dev\advent-of-code-2024')
+
+# from utils.maths import quicksort
+from utils.read_input import read_input, read_input_raw
+
+# raw_text = read_input_raw('./9/example2.txt')
+# text = read_input('./9/example2.txt')
+# raw_text = read_input_raw('./9/example.txt')
+# text = read_input('./9/example.txt')
+raw_text = read_input_raw('./9/input.txt')
+text = read_input('./9/input.txt')
+
+# Part 1
+pt1_value = 0
+
+
+def pt1():
+    """Wrapper for part 1 to lexically contain variables."""
+    value = 0
+    idx1 = 0
+
+    sequence = []
+    file_id = 0
+    type_flag = 0
+
+    while idx1 < len(text[0]):
+        num = int(text[0][idx1])
+        # print('----')
+        # print('file_id, num: ', file_id, num)
+        if type_flag == 0:
+            # print('creating range for ', file_id, ' this many times: ', num)
+            for i in range(num):
+                sequence.append(str(file_id))
+            type_flag = 1
+            file_id += 1
+        elif type_flag == 1:
+            # print('creating spaces: ', num)
+            for i in range(num):
+                sequence.append('.')
+            type_flag = 0
+        idx1 += 1
+
+    sequence_str = ''.join(sequence)
+    print(sequence)
+
+    pointer = len(sequence) - 1
+
+    while pointer >= 0 and not re.search('^\d+\.+$', sequence_str):
+        move_ptr = 0
+        while sequence[move_ptr] != '.' and move_ptr <= len(sequence) - 1:
+            move_ptr += 1
+        sequence[pointer], sequence[move_ptr] = sequence[move_ptr], sequence[pointer]
+        sequence_str = ''.join(sequence)
+        # print(sequence_str)
+        pointer -= 1
+
+    print(sequence_str)
+    for char_i, char in enumerate(sequence):
+        if char == '.':
+            break
+        result = int(char) * char_i
+        # print(char_i, ' * ', char, ' = ',result)
+        value += result
+    return value
+
+
+pt1_value = pt1()
+
+# Part 2
+print('================================ PT2')
+pt2_value = 0
+
+
+def pt2():
+    """Wrapper for part 2 to lexically contain variables."""
+    value = 0
+    idx1 = 0
+
+    sequence = []
+    file_id = 0
+    type_flag = 0
+
+    while idx1 < len(text[0]):
+        num = int(text[0][idx1])
+        if type_flag == 0:
+            for i in range(num):
+                sequence.append(str(file_id))
+            type_flag = 1
+            file_id += 1
+        elif type_flag == 1:
+            for i in range(num):
+                sequence.append('.')
+            type_flag = 0
+        idx1 += 1
+
+    sequence_str = ''.join(sequence)
+    print(sequence)
+
+    pointer = len(sequence) - 1
+
+    while pointer >= 0 and not re.search('^\d+\.+$', sequence_str):
+        move_ptr = 0
+        while sequence[move_ptr] != '.' and move_ptr <= len(sequence) - 1:
+            move_ptr += 1
+        sequence[pointer], sequence[move_ptr] = sequence[move_ptr], sequence[pointer]
+        sequence_str = ''.join(sequence)
+        # print(sequence_str)
+        pointer -= 1
+
+    print(sequence_str)
+    for char_i, char in enumerate(sequence):
+        if char == '.':
+            break
+        result = int(char) * char_i
+        # print(char_i, ' * ', char, ' = ',result)
+        value += result
+    return value
+
+
+pt2_value = pt2()
+
+print('Part 1 Total: ', pt1_value)
+print('Part 2 Total: ', pt2_value)
