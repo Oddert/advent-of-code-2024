da64f344ff13883309af1085ffeba8654481f649
Robyn Felicity Heather Veitch <robynfhveitch@gnail.com>
2024-12-18 20:33:17 +0000

    shashing change attempt for day 9 p2

--- 9/index.py
+++ 9/index.py
@@ -12,10 +12,10 @@ from utils.read_input import read_input, read_input_raw
 
 # raw_text = read_input_raw('./9/example2.txt')
 # text = read_input('./9/example2.txt')
-# raw_text = read_input_raw('./9/example.txt')
-# text = read_input('./9/example.txt')
-raw_text = read_input_raw('./9/input.txt')
-text = read_input('./9/input.txt')
+raw_text = read_input_raw('./9/example.txt')
+text = read_input('./9/example.txt')
+# raw_text = read_input_raw('./9/input.txt')
+# text = read_input('./9/input.txt')
 
 # Part 1
 pt1_value = 0
@@ -102,20 +102,36 @@ def pt2():
 
     sequence_str = ''.join(sequence)
     print(sequence)
+    
+    blocks = re.findall(r'0+|1+|2+|3+|4+|5+|6+|7+|8+|9+|\.+', sequence_str)
+    print(blocks)
 
-    pointer = len(sequence) - 1
+    block_len = len(blocks)
 
-    while pointer >= 0 and not re.search('^\d+\.+$', sequence_str):
-        move_ptr = 0
-        while sequence[move_ptr] != '.' and move_ptr <= len(sequence) - 1:
-            move_ptr += 1
-        sequence[pointer], sequence[move_ptr] = sequence[move_ptr], sequence[pointer]
-        sequence_str = ''.join(sequence)
-        # print(sequence_str)
-        pointer -= 1
+    print('TEST', [(x.start(0), x.end(0)) for x in re.finditer(r'0+|1+|2+|3+|4+|5+|6+|7+|8+|9+|\.+', sequence_str)])
+
+    for idx in range(block_len):
+        block = blocks[block_len - idx - 1]
+        if re.match(r'\.+', block):
+            continue
+        print('----------')
+        print('block', block, 'idx', idx)
+        pointer = 0
+        done = False
+        while pointer < len(blocks) and pointer < block_len - idx and not done:
+            if re.match(r'\.+', blocks[pointer]) and len(blocks[pointer]) >= len(block):
+                print('space found at: ', pointer, ' swaping: ', blocks[block_len - idx - 1], blocks[pointer])
+                blocks[block_len - idx - 1], blocks[pointer] = blocks[pointer], blocks[block_len - idx - 1]
+                done = True
+                sequence_str = ''.join(blocks)
+                blocks = re.findall(r'0+|1+|2+|3+|4+|5+|6+|7+|8+|9+|\.+', sequence_str)
+                print(blocks)
+            pointer += 1
+    
+    print(blocks)
 
     print(sequence_str)
-    for char_i, char in enumerate(sequence):
+    for char_i, char in enumerate(re.findall('.', sequence_str)):
         if char == '.':
             break
         result = int(char) * char_i
